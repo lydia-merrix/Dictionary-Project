e8766e74fc13f1fbdb4f02528aad858a93fdb38e
lydia merrix <lydiamerrix@gmail.com>
2021-12-22 22:32:58 -0500

    fixed url and added axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -10,17 +10,20 @@ export default function Dictionary() {
   }
 
   function handleResponse(response) {
-    console.log(response);
+    console.log(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+    
 
-    let apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/sunset";
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
