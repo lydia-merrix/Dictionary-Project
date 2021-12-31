49a2f6a2c1f4d9838c2d3f55083286e5132a8a4d
lydia merrix <lydiamerrix@gmail.com>
2021-12-31 14:02:18 -0500

    updated and corrected spelling errors

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,18 +1,21 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partofSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) 
-      {
+      {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <p>
+              <strong>Definition:</strong>
               {definition.definition}
               <br />
+              <strong>Example:</strong>
               <em>{definition.example}</em>
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
---
+++ src/Synonyms.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Synonyms(props) {
+  if (props.Synonyms) {
+    return "Hello from Synonyms";
+  } else {
+    return null;
+  }
+}
