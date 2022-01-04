0907063307bfa738d0dc18253a0025a230bf3865
lydia merrix <lydiamerrix@gmail.com>
2022-01-03 22:18:09 -0500

    added components for Phonetic d

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,10 +2,10 @@ import React from "react";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
+  
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partofSpeech}</h3>
+      <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
