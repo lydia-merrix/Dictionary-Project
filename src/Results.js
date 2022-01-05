201d87739aded3689d89f5e71c916a824d63d5ec
lydia merrix <lydiamerrix@gmail.com>
2022-01-05 14:42:48 -0500

    Update Results.js

--- src/Results.js
+++ src/Results.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Meaning from "./Meaning";
 import Phonetic from "./Phonetic";
-import "./Results.css"
+import "./Results.css";
 
 export default function Results(props) {
   if (props.results) {
