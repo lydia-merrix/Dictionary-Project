0579e797cfc7441101c817ffbc9af5f97208f200
lydia merrix <lydiamerrix@gmail.com>
2022-01-05 20:02:52 -0500

    updated wording for Synonym

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Synonyms.css";
 
 export default function Synonyms(props) {
-  if (props.Synonyms) {
+  if (props.synonyms) {
     return (
       <ul className="Synonyms">
         {props.synonyms.map(function (synonym, index) {
