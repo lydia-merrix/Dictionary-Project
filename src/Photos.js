e93c1df6249f3467050dccf3a9a4d4532692dea7
lydia merrix <lydiamerrix@gmail.com>
2022-01-05 19:19:40 -0500

    updated wording for export and alt

--- src/Photos.js
+++ src/Photos.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Photos.css";
 
-export default function (props) {
+export default function Photos(props) {
   if (props.photos) {
     return (
       <section className="Photos">
@@ -10,7 +10,11 @@ export default function (props) {
             return (
               <div className="col-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img src={photo.src.landscape} className="img-fluid" />
+                  <img
+                    src={photo.src.landscape}
+                    className="img-fluid"
+                    alt={photo.alt}
+                  />
                 </a>
               </div>
             );
